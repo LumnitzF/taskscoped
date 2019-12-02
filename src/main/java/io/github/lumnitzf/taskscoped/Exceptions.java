c855b44de76a1e5645409fe54d650bbe370bcca6
Fritz Lumnitz <fritz.lumnitz@tum.de>
2019-12-02 20:31:14 +0100

    Remove unused Exceptions

--- src/main/java/io/github/lumnitzf/taskscoped/Exceptions.java
+++ src/main/java/io/github/lumnitzf/taskscoped/Exceptions.java
@@ -1,10 +1,6 @@
 package io.github.lumnitzf.taskscoped;
 
 import javax.enterprise.context.ContextNotActiveException;
-import javax.enterprise.context.Dependent;
-import javax.enterprise.inject.spi.Bean;
-import javax.enterprise.inject.spi.DefinitionException;
-import java.util.concurrent.ExecutorService;
 
 class Exceptions {
 
@@ -13,13 +9,5 @@ class Exceptions {
 
     static ContextNotActiveException taskScopeNotActive() {
         return new ContextNotActiveException("TaskScope is not active");
-    }
-
-    static DefinitionException beanNotOfTypeExecutorService(Bean<?> bean) {
-        return new DefinitionException(bean + " must be of type " + ExecutorService.class);
-    }
-
-    static DefinitionException beanNotDependentScoped(Bean<?> bean) {
-        return new DefinitionException(bean + " must have scope " + Dependent.class);
     }
 }
